723586a5ee175d05be32d38eb9331bb0bf9c52ca
Hairul008 <hairullabri5@gmail.com>
2023-12-16 22:52:22 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "F55122008 Percobaan 6"
+include(":app")
